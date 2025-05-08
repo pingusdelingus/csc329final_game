7322a47bb3df9e492a896f6d294f9b721f4a3660
pingusdelingus <alphaspammail2016@gmail.com>
2025-05-08 17:26:40 -0400

    more scripts

---
+++ scripts/coinScripts/coinCounter.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class coinCounter : MonoBehaviour
+{
+    public int numOfCoins = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
